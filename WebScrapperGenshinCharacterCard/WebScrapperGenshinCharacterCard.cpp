6270cdda82a2ae8b1b2e75f7905f02e5bda685d7
AFOEK <felix.montalfu@gmail.com>
2022-10-07 15:01:24 +0700

    Adding Csv Extractor, fixing confused std namespace, and removing unnessary library

--- WebScrapperGenshinCharacterCard/WebScrapperGenshinCharacterCard.cpp
+++ WebScrapperGenshinCharacterCard/WebScrapperGenshinCharacterCard.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include "cpr/cpr.h"
 #include "gumbo.h"
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 //This is an example static links assets of character card image:
 //https://static.wikia.nocookie.net/gensin-impact/images/f/f8/Character_Albedo_Card.png
@@ -14,25 +15,39 @@
 //Tutorial link:
 //https://www.webscrapingapi.com/c-web-scraping/
 
-using namespace std;
-using namespace cpr;
+std::string root_url = "https://genshin-impact.fandom.com/";
+std::ofstream writeCsv("FileName.csv");
+std::ofstream writeImgLink("FileImg.txt");
+std::ifstream readCsv("FileName.csv");
 
-ofstream writeCsv("FileName.csv");
+struct data {
+    std::string type, file_name, link;
+};
 
-string extract_html_page() {
-    Url url = Url{"https://genshin-impact.fandom.com/wiki/Category:Character_Cards"};
-    Response res = Get(url);
+std::string extract_html_page_category() {
+    cpr::Url url_category = cpr::Url{root_url+"wiki/Category:Character_Cards"};
+    cpr::Response res = Get(url_category);
     return res.text;
 }
 
-void check_csv_exist(const string &filename) {
-    ifstream fin(filename);
-    if (fin.fail()) {
-        return;
+std::string extract_html_page_character(std::string character_wiki_link) {
+    cpr::Url url_character = cpr::Url{root_url+character_wiki_link};
+    cpr::Response res = Get(url_character);
+    return res.text;
+}
+
+std::vector<data> extract_character_link() {
+    std::vector<data> Data;
+    std::string line;
+    while (std::getline(readCsv, line, ',')) {
+        std::stringstream lineStream(line);
+        data ele;
+        lineStream >> ele.type >> ele.file_name >> ele.link;
+        Data.push_back(ele);
+        std::getline(readCsv, line);
     }
-    else {
-        remove("FileName.csv");
-    }
+    readCsv.close();
+    return Data;
 }
 
 void search_for_a_name(GumboNode* node) {
@@ -44,9 +59,9 @@ void search_for_a_name(GumboNode* node) {
         GumboAttribute* title = gumbo_get_attribute(&node->v.element.attributes, "title");
         GumboAttribute* href = gumbo_get_attribute(&node->v.element.attributes, "href");
         if (title && href) {
-            cout << title->value << " " << href->value << "\n";
-            string FileName = title->value;
-            string LinkStr = href->value;
+            std::cout << title->value << " " << href->value << "\n";
+            std::string FileName = title->value;
+            std::string LinkStr = href->value;
             if (FileName.rfind("File:") == 0) {
                 writeCsv << "File," << FileName << "," << LinkStr << "\n";
             }
@@ -64,8 +79,7 @@ void search_for_a_name(GumboNode* node) {
 
 
 int main() {
-    check_csv_exist("FileName.csv");
-    string page_content = extract_html_page();
+    std::string page_content = extract_html_page_category();
     GumboOutput* parsed_res = gumbo_parse(page_content.c_str());
     writeCsv << "Type,FileName,InnerLink" << "\n";
     search_for_a_name(parsed_res->root);
