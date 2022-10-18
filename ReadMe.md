6bdf964b29377b5dbb92503f893f458310c6dda4
AFOEK <felix.montalfu@gmail.com>
2022-10-18 22:42:56 +0700

    Adding more ReadMe context, fixing memory leaks (due opened file stream), adding comment in various part, adding download function (-cUrl) and minor fix for HD Image

--- ReadMe.md
+++ ReadMe.md
@@ -1,11 +1,32 @@
-﻿<pre>
+﻿## Intro
+
+This is a console programme which using `Gumbo`, `cpr`, and `cUrl` for scrape all character card image from Genshin Impact Wiki Fandom, as [Tree Directory below](##Web-Tree-Directory), all image file is contained into a CDN server. For the image it's always get latest version and uncroped version.
+
+## Web Tree Directory
+
+<pre>
 Genshin Wiki Fandom (https://genshin-impact.fandom.com/wiki/Genshin_Impact_Wiki)
 └── ...
     └── Wiki Fandom Category Page (https://genshin-impact.fandom.com/wiki/Category:Character_Cards)
         └── Character Page (https://genshin-impact.fandom.com/wiki/*Character_Name*)
             ├── ...
-            ├── Character Card Image (Static_CDN_File)
-            ├── Character Wish  Image (Static_CDN_File)
+            ├── Character Card Image (Static_CDN_File [https://static.wikia.nocookie.net/gensin-impact/images/8/8d/Character_Ganyu_Card.png/revision/latest/])
+            ├── Character Wish Image (Static_CDN_File [https://static.wikia.nocookie.net/gensin-impact/images/8/8d/Character_Ganyu_Wish.png/revision/latest/])
             └── ...
 </pre>
-ASCII Tree Directory powered by: [ASCII Tree Generator](https://codepen.io/weizhenye/details/eoYvye)
+
+## To-Do
+
+- [x] Scrap all character link from `/wiki/Category:Character_Card`.
+- [x] Scrap all character card images link from each character links from file.
+- [x] Get character images card HD and uncropped.
+- [ ] Download all character card images.
+- [ ] Get character images wish HD and uncropped.
+- [ ] Download all character card images.
+- [ ] Compile for Linux (x86 and x64), macOS (x64 and armhf), and ARM(arm64 and armv7l).
+
+## Citation
+
+ASCII Tree Directory powered by: [ASCII Tree Generator](https://codepen.io/weizhenye/details/eoYvye).   
+This project powered by: [gumbo](https://github.com/google/gumbo-parser), [cpr](https://github.com/libcpr/cpr), [cUrl](https://curl.se/libcurl/) and [C++20](https://isocpp.org/std/the-standard).   
+Data sources: [genshin wiki fandom](https://genshin-impact.fandom.com/wiki/Genshin_Impact_Wiki) 
--- WebScrapperGenshinCharacterCard/WebScrapperGenshinCharacterCard.cpp
+++ WebScrapperGenshinCharacterCard/WebScrapperGenshinCharacterCard.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <curl/curl.h>
 #include <curl/easy.h>
+#include <filesystem>
 #include "cpr/cpr.h"
 #include "gumbo.h"
 
@@ -25,8 +26,7 @@ std::ofstream writeCsv("FileName.csv");
 std::ofstream writeImgLink("FileImg.txt");
 std::ofstream writeLink("ImgLink.txt");
 std::ifstream readCsv("FileName.csv");
-std::ifstream readImgLink("FileImg.txt");
-std::ifstream readLink("FileImg.txt");
+std::ifstream readLink("ImgLink.txt");
 
 std::string extract_html_page_category() {
     cpr::Url url_category = cpr::Url{root_url+"/wiki/Category:Character_Cards"};
@@ -68,6 +68,7 @@ std::vector<std::string> extract_character_link() {
         data_img.push_back(data[row][colNum]);
     }
     readCsv.close();
+    writeImgLink.close();
     return data_img;
 }
 
@@ -90,8 +91,8 @@ void search_for_img(GumboNode* node) {
             std::string LinkImg;
             std::string LinkImgTmp = imgLink->value;
             if (LinkImgTmp.rfind("_Card") != 18446744073709551615) {
-                //writeLink << LinkImg << "\n";
                 LinkImgTmp.erase(LinkImgTmp.end() - 41, LinkImgTmp.end());
+                writeLink << LinkImgTmp << "\n";
                 std::cout << LinkImgTmp << "\n";
             }
         }
@@ -128,13 +129,29 @@ void search_for_a_name(GumboNode* node) {
     }
 }
 
+void downloads_images() {
+    std::filesystem::create_directory("Character Genshin Card Image");
+    std::string line;
+    std::vector<std::string> img_links;
+    while (std::getline(readLink, line)) {
+        std::istringstream ISS;
+        img_links.push_back(line);
+    }
+
+    for (int i = 0; i < img_links.size();i++) {
+        std::cout << img_links[i] << "\n";
+    }
+}
+
 int main() {
+    //Get character list from /wiki/Category:Character_Cards
     std::vector<std::string> img_vecs, temp;
     std::string page_content = extract_html_page_category();
     GumboOutput* parsed_res = gumbo_parse(page_content.c_str());
     search_for_a_name(parsed_res->root);
     writeCsv.close();
     gumbo_destroy_output(&kGumboDefaultOptions, parsed_res);
+    //Get character link based character category
     temp = extract_character_link();
     img_vecs = sanitize_vecs(temp);
     for (int i = 8; i < img_vecs.size()-3; i++) {
@@ -149,5 +166,9 @@ int main() {
     search_for_img(parsed_res_chara->root);
     gumbo_destroy_output(&kGumboDefaultOptions, parsed_res_chara);*/
     writeLink.close();
+    //Download Img
+    downloads_images();
+    readLink.close();
+    //Exit gracefully and return memory to OS
     return 0;
 }
