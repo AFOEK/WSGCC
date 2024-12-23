63b6d530db7228f66e594b1958156511f901e888
AFOEK <felix.montalfu@gmail.com>
2024-12-23 20:14:38 +0700

    Fixing x86 and x64 cmake build (repaired)

--- WebScrapperGenshinCharacterCard/CMakeToolkits/linux_x64.cmake
+++ WebScrapperGenshinCharacterCard/CMakeToolkits/linux_x64.cmake
@@ -3,7 +3,9 @@ if(NOT DEFINED ENV{VCPKG_ROOT})
 endif()
 
 set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "vcpkg toolchain file" FORCE)
-set(VCPKG_TARGET_TRIPLET x64-linux CACHE STRING "vcpkg target triplet")
+set(VCPKG_TARGET_TRIPLET "x64-linux" CACHE STRING "vcpkg target triplet")
 set(TARGET_ARCH "linux_x64" CACHE STRING "Target Architecture")
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_CXX_FLAGS "-std=c++20")
+
+set(CMAKE_PREFIX_PATH "$ENV{VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}")
