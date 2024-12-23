efd267fc6b8a0769b145b526b506b6a2cc40de1c
AFOEK <felix.montalfu@gmail.com>
2024-12-23 21:42:09 +0700

    Updated ARM build (aarch64 and arm32)

---
+++ WebScrapperGenshinCharacterCard/CMakeToolkits/linux_arm32.cmake
@@ -0,0 +1,17 @@
+if(NOT DEFINED ENV{VCPKG_ROOT})
+    message(FATAL_ERROR "VCPKG_ROOT environment variable is not set. Please set it to the vcpkg installation path.")
+endif()
+
+set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "vcpkg toolchain file" FORCE)
+set(VCPKG_TARGET_TRIPLET "arm-linux" CACHE STRING "vcpkg target triplet")
+set(CMAKE_SYSTEM_PROCESSOR "arm" CACHE STRING "Target Binary")
+set(CMAKE_SYSTEM_NAME Linux)
+set(TARGET_ARCH "arm32" CACHE STRING "Target Architecture")
+set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_CXX_FLAGS "-std=c++20")
+
+set(CMAKE_PREFIX_PATH "$ENV{VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}")
